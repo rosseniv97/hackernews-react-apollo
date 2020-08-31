a466168acb015fe59c42199513a3d31ccd2545c4
Rosen Ivanov <rivanov@cleancodefactory.de>
2020-08-31 17:11:57 +0300

    Http headers set for authentication

--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
